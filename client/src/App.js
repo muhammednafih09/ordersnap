21c996183605f60cf3a687c6cb699e36a2eb112c
Muhammed Nafih <muhammednafih09@gmail.com>
2023-09-01 12:28:31 +0530

    bootstrap added

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <p className="text-danger">testing bootstrap</p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
